8819c24a44ef8ae396a318d19043456d0bb9389a
Vitor Zachi Junior <vitorzachi@gmail.com>
2017-10-16 00:38:31 -0200

    Update AllThatImLivingFor.java

--- AllThatImLivingFor.java
+++ AllThatImLivingFor.java
@@ -9,7 +9,7 @@ import org.jfugue.player.Player;
 public class AllThatImLivingFor {
 	public static void main(String[] args) {
 		Player player = new Player();
-		
+		System.out.println("begin...");
 		//@formatter:off
 		player.play("V0 I[Piano] " 
 			+ "39i 46i 51i 58q Rq | "
@@ -21,7 +21,24 @@ public class AllThatImLivingFor {
 			+ "35i 42i 47i 58q Rq | "
 			+ "35i 42i 47i 56q Rq | "
 		
+			
+				+ "65q 63i 65q 63i 65q 63i 65i 66q Rq | "
+				+ "65q 63i 65q 63i 65q 63i 65i 66q Rq | "
+				+ "65i 66i 68q 66q 65q Rq | "
+				+ "65i 66i 68q 66q 65q 63q Rq | "
+				
+				+ "65q 63i 65q 63i 65q 63i 65i 66q Rq | "
+				+ "65q 63i 65q 63i 65q 63i 70q 66q Rq | "
+				+ "65i 66i 68q 66q 65q Rq | "
+				+ "65i 66i 68q 66q 65q 63q 63q Rq | "
+					
+					+ "71q 71i 71i 71q 71q 71i 70q Rq | "
+					+ "71q 71i 71i 71q 71q 71i 70q Rq | "
+					+ "71q 71i 71i 71q 73q 70q Rq | "
+					+ "63i 65q 66q 68q 66q 65q 63q Rq | "
+					
 //		@formatter:on
 		);
+		System.out.println("end :)");
 	}
 }
