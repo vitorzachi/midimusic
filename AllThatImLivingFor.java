c25169c9d4d9e0fb806f11762a0ad3d0cf307fe0
Vitor Zachi Junior <vitorzachi@gmail.com>
2017-11-05 13:50:54 -0200

    Update AllThatImLivingFor.java

--- AllThatImLivingFor.java
+++ AllThatImLivingFor.java
@@ -13,23 +13,23 @@ public class AllThatImLivingFor {
 		System.out.println("playing...");
 		//@formatter:off
 		player.play("V0 I[Piano] " 
-			/*+ "39i 46i 51i 58q Rq | "
-			+ "39i 46i 51i 56q Rq | "
-			+ "39i 46i 51i 54q Rq | " 
-			+ "39i 46i 51i 46i 53i 54i 53i 49i | " 
-			+ "35i 42i 47i 54q Rq | "
-			+ "35i 42i 47i 59q Rq | "
-			+ "35i 42i 47i 58q Rq | "
-			+ "35i 42i 47i 56q Rq | "*/
+			/*
+			+ "47i 54i 59i 66q Rq | "
+			+ "47i 54i 59i 64q Rq | "
+			+ "47i 54i 59i 62q Rq | " 
+			+ "47i 54i 59i 54i 61i 62i 61i 57i | " 
 			+ "43i 50i 55i 62q Rq | "
-			+ "43i 50i 55i 60q Rq | "
-			+ "43i 50i 55i 58q Rq | " 
-			+ "43i 50i 55i 50i 57i 58i 57i 53i | " 
-			+ "39i 46i 51i 58q Rq | "
-			+ "39i 46i 51i 63q Rq | "
-			+ "39i 46i 51i 62q Rq | "
-			+ "39i 46i 51i 60q Rq | "
-		
+			+ "43i 50i 55i 67q Rq | "
+			+ "43i 50i 55i 66q Rq | "
+			+ "43i 50i 55i 64q Rq | "*/
+		+ "51i 58i 63i 70q Rq | "
+			+ "51i 58i 63i 68q Rq | "
+			+ "51i 58i 63i 66q Rq | " 
+			+ "51i 58i 63i 58i 65i 66i 65i 61i | " 
+			+ "47i 54i 59i 66q Rq | "
+			+ "47i 54i 59i 71q Rq | "
+			+ "47i 54i 59i 70q Rq | "
+			+ "47i 54i 59i 68q Rq | "
 			
 				+ "65q 63i 65q 63i 65q 63i 65i 66q Rq | "
 				+ "65q 63i 65q 63i 65q 63i 65i 66q Rq | "
